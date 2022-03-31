ff7209df8f687f744887a3f001c711e60c01e91d
SebF33 <78741537+SebF33@users.noreply.github.com>
2022-03-31 14:03:01 +0200

    tailwindcss globals

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary-color': '#837c5e',
+        'primary-color': "var(--primary-color)",
+        'lighter-primary-color': "var(--lighter-primary-color)",
+        'darker-primary-color': "var(--darker-primary-color)",
+        'secondary-color': "var(--secondary-color)",
+        'tertiary-color': "var(--tertiary-color)",
+        'light-color': "var(--light-color)",
+        'error-color': "var(--error-color)",
+        'error-bg-color': "var(--error-bg-color)",
+        'success-color': "var(--success-color)",
+        'success-bg-color': "var(--success-bg-color)",
       },
     }
   },
