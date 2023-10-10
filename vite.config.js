77f257ed9871da849cfc574b7ac4d2cbdee6b5db
SebF33 <78741537+SebF33@users.noreply.github.com>
2023-10-10 09:39:00 +0200

    vite cfg update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          //isCustomElement: (tag) => tag.includes("value-here"),
+        },
+        transformAssetUrls: {
+          // default tags
+          tags: {
+            video: ["src", "poster"],
+            source: ["src"],
+            img: ["src"],
+            image: ["xlink:href", "href"],
+            use: ["xlink:href", "href"],
+          },
+        },
+      },
+    }),
   ],
+  base: "/magicardt/",
+  define: {
+    "process.env": {},
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 })
